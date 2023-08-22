ee2fc9eae77aee0d2f46c1123767f8b810ce6f79
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2023-08-21 22:50:39 -0500

    Replace switch with array in getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,23 +1,24 @@
+const choices = ["ROCK", "PAPER", "SCISSORS"]
+
 function getComputerChoice() {
-  let randomize = Math.floor(Math.random() * 3 + 1);
-  let compChoice;
+ return choices[Math.floor(Math.random() * 3 + 1)];
+}
 
-  switch(randomize) {
-    case 1:
-      compChoice = "Rock";
-      break;
-    
-    case 2:
-      compChoice = "Paper";
-      break;
-    
-    case 3:
-      compChoice = "Scissors";
-      break;
-    
-    default:
-      break;
+function playRound(playerSelection, computerSelection) {
+  let result = "You ";
+
+  playerSelection = playerSelection.toUpperCase();
+  if(playerSelection === computerSelection) {
+    result += "Draw!"
+  } else if (
+    (playerSelection == "ROCK" && computerSelection == "SCISSORS") 
+    || (playerSelection == "PAPER" && computerSelection == "ROCK")
+    || (playerSelection == "SCISSORS" && computerSelection == "PAPER")
+  ) {
+    result  += `Win! ${playerSelection} beats $`
   }
+}
 
-  return compChoice;
+function Capitalize(word) {
+  word = word.charAt(0).toUpperCase() + word.slice(1);
 }
